42ae1e683d2fef5fd86d6a3bad2d1c9aa68e80eb
XavierCruz5106 <superguyman121@gmail.com>
2024-10-30 10:39:11 -0400

    feat: added 3 solutions and writeup for each

--- ProductOfArrayExceptSelf/app/src/main/java/productofarrayexceptself/App.java
+++ ProductOfArrayExceptSelf/app/src/main/java/productofarrayexceptself/App.java
@@ -4,11 +4,63 @@
 package productofarrayexceptself;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+    public int[] solution1(int[] nums) {
+        int[] answer = new int[nums.length];
+        for (int i = 0; i < nums.length; i++){
+            int product = 1;
+            for (int j = 0; j < nums.length; j++){
+                if (i == j){
+                    continue;
+                }
+
+                product *= nums[j];
+            }
+            answer[i] = product;
+        }
+        return answer;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public int[] solution2(int[] nums){
+        int n = nums.length;
+        int[] ans = new int[n];
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        prefix[0] = 1;
+        for (int i = 1; i < n; ++i){
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        suffix[n - 1] = 1;
+        for (int i = n - 2; i >= 0; --i){
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < n; ++i){
+            ans[i] = prefix[i] * suffix[i];
+        }
+
+        return ans;
     }
+
+    public int[] solution3(int[] nums){
+        int[] answer = new int[nums.length];
+        int product = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = product;
+            product *= nums[i];
+        }
+        product = 1;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] *= product;
+            product *= nums[i];
+        }
+
+        return answer;
+    }
+
+
+
 }
--- ProductOfArrayExceptSelf/app/src/test/java/productofarrayexceptself/AppTest.java
+++ ProductOfArrayExceptSelf/app/src/test/java/productofarrayexceptself/AppTest.java
@@ -7,8 +7,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    int[] nums = new int[]{1, 2, 3, 4};
+    int[] expected = new int[]{24,12,8,6};
+    App classToTest = new App();
+
+    @Test void solution1ReturnsProperArray(){
+        assertArrayEquals(expected, classToTest.solution1(nums));
+    }
+
+    @Test void solution2ReturnsProperArray(){
+
+        assertArrayEquals(expected, classToTest.solution2(nums));
+    }
+
+    @Test void solution3ReturnProperArray(){
+        assertArrayEquals(expected, classToTest.solution3(nums));
     }
 }
